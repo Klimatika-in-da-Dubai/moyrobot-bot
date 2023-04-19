2e8ba8b22da6d9c1baeb0cbb88a44c170a6ec04c
GerogeGol <george.gol003@gmail.com>
2023-04-19 18:32:43 +0300

    set type of PK BigInteger for some tables

--- app/services/database/models/group.py
+++ app/services/database/models/group.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.types import BigInteger
 
 
 from app.services.database.base import Base
@@ -7,5 +8,7 @@ from app.services.database.base import Base
 class Group(Base):
     __tablename__ = "tg_group"
 
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True, autoincrement=False)
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, unique=True, autoincrement=False
+    )
     name: Mapped[str]
--- app/services/database/models/mailing.py
+++ app/services/database/models/mailing.py
@@ -1,7 +1,7 @@
 from enum import IntEnum, auto
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.types import Enum
+from sqlalchemy.types import Enum, BigInteger
 
 
 from app.services.database.base import Base
@@ -21,7 +21,10 @@ class Mailing(Base):
     __tablename__ = "mailing"
 
     id: Mapped[int] = mapped_column(
-        ForeignKey("tg_user.id", ondelete="CASCADE"), primary_key=True, unique=False
+        BigInteger,
+        ForeignKey("tg_user.id", ondelete="CASCADE"),
+        primary_key=True,
+        unique=False,
     )
     type: Mapped[MailingType] = mapped_column(
         Enum(MailingType), primary_key=True, unique=False
@@ -32,7 +35,10 @@ class GroupMailing(Base):
     __tablename__ = "group_mailing"
 
     id: Mapped[int] = mapped_column(
-        ForeignKey("tg_group.id", ondelete="CASCADE"), primary_key=True, unique=False
+        BigInteger,
+        ForeignKey("tg_group.id", ondelete="CASCADE"),
+        primary_key=True,
+        unique=False,
     )
     type: Mapped[MailingType] = mapped_column(
         Enum(MailingType), primary_key=True, unique=False
