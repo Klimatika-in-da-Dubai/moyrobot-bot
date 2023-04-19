395cf76f8670054c31f22f443d3a77c758c1d5d6
GerogeGol <george.gol003@gmail.com>
2023-04-19 12:06:29 +0300

    alembic

---
+++ scripts/versions/b9b674c718d0_fix.py
@@ -0,0 +1,24 @@
+"""fix
+
+Revision ID: b9b674c718d0
+Revises: 0c77a03d96fb
+Create Date: 2023-04-18 17:51:48.942674
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9b674c718d0'
+down_revision = '0c77a03d96fb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
