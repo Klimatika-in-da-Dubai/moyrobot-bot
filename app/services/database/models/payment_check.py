544c51b49e4d1addc62dac5d12d8e4bf398957b2
George Golubev <34991190+GerogeGol@users.noreply.github.com>
2023-07-09 17:10:29 +0300

    Revert "Revert "Payment check""

---
+++ app/services/database/models/payment_check.py
@@ -0,0 +1,22 @@
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+from app.services.database.base import Base
+
+
+class PaymentCheck(Base):
+    __tablename__ = "payment_check"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=True,
+    )
+    start_check: Mapped[datetime]
+    end_check: Mapped[datetime]
+    count_manual_starts: Mapped[int]
+    last_notification: Mapped[datetime] = mapped_column(default=datetime.now)
+    count_notifications: Mapped[int] = mapped_column(default=0)
+    checked: Mapped[bool] = mapped_column(default=False)
+    alerted: Mapped[bool] = mapped_column(default=False)
