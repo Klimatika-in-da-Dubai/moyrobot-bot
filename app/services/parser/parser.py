66bafa34adb1ab9c7ce5f74fa35115e2eb51c6bb
GerogeGol <george.gol003@gmail.com>
2024-03-02 15:22:09 +0300

    fix for getting manual starts

--- app/services/parser/parser.py
+++ app/services/parser/parser.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import asyncio
 from datetime import datetime
 
@@ -45,7 +44,7 @@ class Parser:
 
         return [
             self.__parse_row_to_manual_start(terminal_id, row)
-            for row in manual_starts.values
+            for _, row in manual_starts.iterrows()
         ]
 
     def __get_starts_datatframe(self, table_sales_page: str) -> pd.DataFrame:
@@ -58,7 +57,7 @@ class Parser:
         return starts_df.loc[starts_df["Тип запуска  БУМ"] == MANUAL_START_TEXT]
 
     def __parse_row_to_manual_start(
-        self, terminal_id: int, row: np.ndarray
+        self, terminal_id: int, row: pd.Series
     ) -> ManualStart:
         return ManualStart(
             id=self.__get_id(row),
@@ -68,12 +67,12 @@ class Parser:
         )
 
     def __get_id(self, row) -> str:
-        return str(row[1])
+        return row["Id"]
 
     def __get_date(self, row) -> datetime:
-        return datetime.strptime(row[2], "%d.%m.%Y %H:%M:%S")
+        return datetime.strptime(row["Дата"], "%d.%m.%Y %H:%M:%S")
 
     def __get_mode(self, row) -> int | None:
-        if row[8] == "Режим":
+        if row["Режим  БУМ"] == "Режим":
             return None
-        return int(row[8].split()[1])
+        return int(row["Режим  БУМ"].split()[1])
